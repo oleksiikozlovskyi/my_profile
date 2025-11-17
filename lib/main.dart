7a5f276f5b977d77b65b0cff607fb2a51e91dfd4
Zer <lexx8407@gmail.com>
2025-11-17 16:00:58 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'ContactsScreen.dart';
+import 'HomeScreen.dart';
+import 'ProfileScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Мій профіль',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/profile': (context) => const ProfileScreen(),
+        '/contacts': (context) => const ContactsScreen(),
+      },
+    );
+  }
+}
